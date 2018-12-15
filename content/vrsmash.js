992222808bdb7094d27025fa0fe976557bf467a8
Test <test@test.com>
2018-12-15 02:12:35 +0000

    test

---
+++ content/vrsmash.js
@@ -0,0 +1,13 @@
+// Set runonce variable
+
+// Get list of video summary elements
+
+// Scrape meta from video summary elements to object
+
+// Loop through summary elements
+
+// Load video page and complete meta obj
+
+// Pass objects to background script
+
+// Load next page in index (optional)
---
+++ downer.js
@@ -0,0 +1,11 @@
+// Listen for button click
+
+// On click load relevant scraper for page url
+
+// Listen for list of objects from scraper
+
+// On recieving list add to download queue
+
+// Keep an eye on download queue
+
+// When one download finishes start another
